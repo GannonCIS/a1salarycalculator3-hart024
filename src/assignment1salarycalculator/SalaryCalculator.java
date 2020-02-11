e0aefb2f37f74123c50deb733c8abad42e3b0b40
Sean Hart <hart024@gannon.edu>
2020-02-11 11:03:02 -0500

    create salarycalculator class with all necessary methods

---
+++ src/assignment1salarycalculator/SalaryCalculator.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment1salarycalculator;
+
+/**
+ *
+ * @author seanh
+ */
+public class SalaryCalculator {
+   public double calcHourlyWage(int salary){
+       return 2; //CHANGE!!!!!!!!!
+   }
+   
+   public double calcSalary(int hourlyRate){
+       return 2; //CHANGE
+   }
+   
+   public double calcTaxes(double perc){
+       return 2; //change
+   }
+   
+   public double monthlyAfterTax(int salary, double perc){
+       return 2; //change
+   }
+   
+}
